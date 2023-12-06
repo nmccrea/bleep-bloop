bd46ebfa5398305378b56626c7fa9f8b0f4c6e6c
Nicholas McCrea <nicholas.mccrea@colorado.edu>
2023-12-05 23:25:14 -0700

    Initialize MCUXpresso project.

---
+++ board/clock_config.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2017-2023 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * @file    clock_config.h
+ * @brief   Board clocks header file.
+ */
+ 
+/* This is a template for board specific configuration created by MCUXpresso IDE Project Wizard.*/
+
+#ifndef _CLOCK_CONFIG_H_
+#define _CLOCK_CONFIG_H_
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * @brief 	Initialize board clocks.
+ */
+void BOARD_InitBootClocks(void);
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus */
+
+#endif /* _CLOCK_CONFIG_H_ */
+
+
