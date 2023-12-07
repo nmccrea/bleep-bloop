aaef7852c95f0b02d6dbfcf39d3e4a697be517b6
Nicholas McCrea <nicholas.mccrea@colorado.edu>
2023-12-07 16:43:27 -0700

    Format and initialize `main.c`.

--- source/main.c
+++ source/main.c
@@ -1,49 +1,39 @@
-/*
- * Copyright 2016-2023 NXP
- * All rights reserved.
- *
- * SPDX-License-Identifier: BSD-3-Clause
+/**
+ * @brief Application entry point.
  */
 
-/**
- * @file    Screamo.c
- * @brief   Application entry point.
- */
-#include <stdio.h>
+#include "MKL25Z4.h"
 #include "board.h"
+#include "clock_config.h"
+#include "fsl_debug_console.h"
 #include "peripherals.h"
 #include "pin_mux.h"
-#include "clock_config.h"
-#include "MKL25Z4.h"
-#include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
+#include <stdio.h>
 
-/* TODO: insert other definitions and declarations here. */
-
-/*
- * @brief   Application entry point.
+/**
+ * @brief Application entry point.
  */
-int main(void) {
-
-    /* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
+int main(void)
+{
+  /* Init board hardware. */
+  BOARD_InitBootPins();
+  BOARD_InitBootClocks();
+  BOARD_InitBootPeripherals();
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
+  /* Init FSL debug console. */
+  BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\r\n");
+  PRINTF("Hello World\r\n");
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+  /* Force the counter to be placed into memory. */
+  volatile static int i = 0;
+  /* Enter an infinite loop, just incrementing a counter. */
+  while (1) {
+    i++;
+    /* 'Dummy' NOP to allow source level single stepping of
+        tight while() loop */
+    __asm volatile("nop");
+  }
+  return 0;
 }
