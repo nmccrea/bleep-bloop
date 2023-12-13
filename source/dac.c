4fa28819b0e26f4f87d8e78c809684f35cf0c40d
Nicholas McCrea <nicholas.mccrea@colorado.edu>
2023-12-12 20:38:37 -0700

    Port the DAC module from assignment 7.

---
+++ source/dac.c
@@ -0,0 +1,125 @@
+/**
+ * @brief This file implements interfaces for operating the digital-analog converter (DAC).
+ */
+
+#include "dac.h"
+#include "MKL25Z4.h"
+#include "fsl_clock.h"
+#include "systick.h"
+#include <stdint.h>
+
+#define DAC_PORTE_OUTPUT_PIN (30U)
+#define DAC_PORTE_OUTPUT_PIN_MUX (0)
+
+#define DMA_DCR_SSIZE_16_BITS (2U)
+#define DMA_DCR_DSIZE_16_BITS (2U)
+#define DMAMUX_CHCFG_SOURCE_TPM0_OVERFLOW (54U)
+
+// TPM prescale selection
+#define DAC_TPM_CLOCK_PRESCALE (0)
+
+// An idle signal sample buffer
+DacSamples_t idle_samples = {.buffer = {0}, .length = DAC_SAMPLE_BUFFER_CAPACITY};
+
+/** The current source of signal samples. */
+DacSamples_t* source = &idle_samples;
+
+void Dac_Initialize()
+{
+  // Clock Port E, DAC0, TPMO, DMAMUX, and DMA
+  SIM->SCGC5 |= SIM_SCGC5_PORTE(1);
+  SIM->SCGC6 |= SIM_SCGC6_DAC0(1) | SIM_SCGC6_TPM0(1) | SIM_SCGC6_DMAMUX(1);
+  SIM->SCGC7 |= SIM_SCGC7_DMA(1);
+
+  // Connect the DAC output pin
+  PORTE->PCR[DAC_PORTE_OUTPUT_PIN] = PORT_PCR_MUX(DAC_PORTE_OUTPUT_PIN_MUX);
+
+  // Turn off DAC buffered mode
+  DAC0->C1 = 0;
+  DAC0->C2 = 0;
+
+  // Enable the DAC and use VDDA as reference voltage
+  DAC0->C0 = (DAC_C0_DACEN(1) | DAC_C0_DACRFS(1));
+
+  // Disable DMA channel 0 for configuration
+  DMAMUX0->CHCFG[0] = 0;
+
+  // Configure the DMA channel 0 interrupt, peripheral requests, cycle stealing, source incrementing
+  // and transfer size
+  DMA0->DMA[0].DCR = DMA_DCR_EINT(1) | DMA_DCR_ERQ(1) | DMA_DCR_CS(1) | DMA_DCR_SINC(1) |
+                     DMA_DCR_SSIZE(DMA_DCR_SSIZE_16_BITS) | DMA_DCR_DSIZE(DMA_DCR_DSIZE_16_BITS);
+
+  // Enable DMA completion interrupts
+  NVIC_SetPriority(DMA0_IRQn, 2);
+  NVIC_ClearPendingIRQ(DMA0_IRQn);
+  NVIC_EnableIRQ(DMA0_IRQn);
+
+  // Set the TPM clock source to be the MCGFLLCLK (48MHz)
+  SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
+  SIM->SOPT2 |= (SIM_SOPT2_TPMSRC(1) | SIM_SOPT2_PLLFLLSEL(1));
+
+  // Configure the TPM period. With a 48 MHz counter and targeting a 96 kHz sample rate, this gives
+  // a period of 48 MHz / 96 kHz = 500 ticks
+  TPM0->MOD = (CLOCK_GetCoreSysClkFreq() / DAC_SAMPLE_FREQUENCY) - 1;
+
+  // Start the TPM counter with the appropriate prescaler and DMA requests enabled
+  TPM0->SC = TPM_SC_CMOD(1) | TPM_SC_PS(DAC_TPM_CLOCK_PRESCALE) | TPM_SC_DMA(1);
+}
+
+void Dac_WaveformToSamples(int32_t (*waveform_function)(int32_t),
+                           uint16_t waveform_domain_period,
+                           uint16_t output_frequency,
+                           uint16_t sample_offset,
+                           DacSamples_t* samples)
+{
+  samples->sample_period = (uint16_t)((uint32_t)DAC_SAMPLE_FREQUENCY / (uint32_t)output_frequency);
+
+  // Here we utilize integer truncation. `samples->length` will be less than or equal to
+  // `DAC_SAMPLE_BUFFER_CAPACITY`.
+  uint16_t number_of_signal_periods = DAC_SAMPLE_BUFFER_CAPACITY / samples->sample_period;
+  samples->length = number_of_signal_periods * samples->sample_period;
+
+  // With integer division, we must divide the waveform input value each time to avoid accumulating
+  // error.
+  int32_t numerator = 0;
+  for (uint16_t i = 0; i < samples->length; ++i) {
+    samples->buffer[i] =
+        sample_offset + waveform_function(numerator / (int32_t)samples->sample_period);
+    numerator += waveform_domain_period;
+  }
+}
+
+void Dac_SetSource(DacSamples_t* samples)
+{
+  source = samples;
+}
+
+void Dac_Play()
+{
+  // Configure the source and destination addresses
+  DMA0->DMA[0].SAR = (uint32_t)source->buffer;
+  DMA0->DMA[0].DAR = (uint32_t)(&(DAC0->DAT[0].DATL));
+
+  // Configure the number of bytes to transfer
+  DMA0->DMA[0].DSR_BCR = DMA_DSR_BCR_BCR(source->length);
+
+  // Enable the DMA channel with TPM0 requests
+  DMAMUX0->CHCFG[0] |=
+      (DMAMUX_CHCFG_ENBL(1) | DMAMUX_CHCFG_SOURCE(DMAMUX_CHCFG_SOURCE_TPM0_OVERFLOW));
+}
+
+/**
+ * @brief Resets the DMA to replay the buffered samples each time the previous DMA transfer
+ * completes.
+ */
+void DMA0_IRQHandler()
+{
+  // Disable further DMA requests until the DMA has been reset
+  DMAMUX0->CHCFG[0] = 0;
+
+  // Clear the Done flag
+  DMA0->DMA[0].DSR_BCR |= DMA_DSR_BCR_DONE(1);
+
+  // Start the next DMA playback
+  Dac_Play();
+}
---
+++ source/dac.h
@@ -0,0 +1,62 @@
+/**
+ * @brief This header declares interfaces for operating the digital-analog converter (DAC).
+ */
+
+#ifndef __DAC_H__
+#define __DAC_H__
+
+#include <stdint.h>
+
+// DAC sample frequency in Hz
+#define DAC_SAMPLE_FREQUENCY (96000U)
+#define DAC_SAMPLE_BUFFER_CAPACITY (1024)
+
+/** A DAC sample buffer and associated properties. */
+typedef struct {
+  // The sample buffer
+  uint16_t buffer[DAC_SAMPLE_BUFFER_CAPACITY];
+
+  // The number of samples currently held in the buffer
+  uint16_t length;
+
+  // The number of samples per period of the signal
+  uint16_t sample_period;
+} DacSamples_t;
+
+/**
+ * @brief Initializes the DAC module and enables periodic sample transfers with DMA.
+ */
+void Dac_Initialize();
+
+/**
+ * @brief Writes a waveform into a DAC sample buffer.
+ *
+ * @param waveform_function The waveform function.
+ * @param waveform_domain_period The fundamental period of the waveform in the function's input
+ * units. That is, the non-negative input value at which the waveform function first repeats.
+ * @param output_frequency The desired analog signal frequency in Hz.
+ * @param sample_offset An offset value that will be added to the output value of the waveform
+ * function before writing to the sample buffer.
+ * @param samples The DAC samples object into which the samples and metadata will be written.
+ */
+void Dac_WaveformToSamples(int32_t (*waveform_function)(int32_t),
+                           uint16_t waveform_domain_period,
+                           uint16_t output_frequency,
+                           uint16_t sample_offset,
+                           DacSamples_t* samples);
+
+/**
+ * @brief Sets the DAC sample source buffer. If the DAC is already playing, the new source will
+ * begin playing once any current DMA transfer is complete.
+ *
+ * @param samples The DAC samples object that will become the new signal source.
+ */
+void Dac_SetSource(DacSamples_t* samples);
+
+/**
+ * @brief Begins playing out the signal provided in the configured sample source buffer. The signal
+ * in the buffer will be looped over indefinitely.
+ */
+void Dac_Play();
+
+#endif
