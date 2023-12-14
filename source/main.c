d20a5f5490907259dbec4ebc89ec800a24a29560
Nicholas McCrea <nicholas.mccrea@colorado.edu>
2023-12-13 20:23:25 -0700

    Extract the main loop into a function.

--- source/main.c
+++ source/main.c
@@ -22,6 +22,53 @@
 #define SCREAMO_MAX_OUTPUT_FREQUENCY (2000)
 
 /**
+ * @brief Runs the main application loop.
+ */
+void Run()
+{
+  // Precalculate constants for converting accelerometer magnitude to frequency
+  const uint32_t magnitude_units_per_frequency_unit =
+      ACCELEROMETER_MAX_MAGNITUDE / (SCREAMO_MAX_OUTPUT_FREQUENCY - SCREAMO_MIN_OUTPUT_FREQUENCY);
+  const uint32_t magnitude_threshold = (ACCELEROMETER_UNITS_PER_G * 10) / 8; // 1.2 Gs
+
+  // Initialize variables
+  DacSamples_t audio_buffer_a, audio_buffer_b;
+  DacSamples_t* audio_buffer_next = &audio_buffer_a;
+  uint16_t frequency;
+  ticktime_t next_tick_time = SysTick_Now();
+  AccelerometerData_t accelerometer_data;
+
+  // Run the main loop
+  while (1) {
+    // Read the accelerometer data
+    Accelerometer_Read(&accelerometer_data);
+
+    // If the magnitude is below the threshold, regail us with silence
+    if (accelerometer_data.magnitude < magnitude_threshold) Dac_Stop();
+    // Otherwise, bleep bloop
+    else {
+      // Calculate the frequency to play
+      frequency = DAC_MIN_OUTPUT_FREQUENCY + ((accelerometer_data.magnitude - magnitude_threshold) /
+                                              magnitude_units_per_frequency_unit);
+
+      // Generate the next waveform
+      audio_buffer_next =
+          (audio_buffer_next == &audio_buffer_a) ? &audio_buffer_b : &audio_buffer_a;
+      Dac_WaveformToSamples(
+          Waveform_Square, SQUARE_DOMAIN_PERIOD, frequency, SQUARE_SCALE_FACTOR, audio_buffer_next);
+
+      // Play the next waveform
+      Dac_SetSource(audio_buffer_next);
+      Dac_Play();
+    }
+
+    // Wait until the next tick
+    next_tick_time += SYSTEM_PERIOD_MS;
+    BusyWaitUntil(next_tick_time);
+  }
+}
+
+/**
  * @brief Application entry point.
  */
 int main(void)
@@ -45,42 +92,7 @@ int main(void)
   PRINTF("\r\n");
   PRINTF("Screamo started\r\n");
 
-  const uint32_t magnitude_units_per_frequency_unit =
-      ACCELEROMETER_MAX_MAGNITUDE / (SCREAMO_MAX_OUTPUT_FREQUENCY - SCREAMO_MIN_OUTPUT_FREQUENCY);
-  const uint32_t magnitude_threshold = (ACCELEROMETER_UNITS_PER_G * 10) / 8; // 1.2 Gs
-
-  DacSamples_t audio_buffer_a, audio_buffer_b;
-  DacSamples_t* audio_buffer_next = &audio_buffer_a;
-  uint16_t frequency;
-  ticktime_t next_tick_time = SysTick_Now();
-  AccelerometerData_t accelerometer_data;
-  while (1) {
-    // Read the accelerometer data
-    Accelerometer_Read(&accelerometer_data);
-
-    if (accelerometer_data.magnitude < magnitude_threshold) {
-      // If the magnitude is below the threshold, don't play anything
-      Dac_Stop();
-    } else {
-      // Calculate the frequency to play
-      frequency = DAC_MIN_OUTPUT_FREQUENCY + ((accelerometer_data.magnitude - magnitude_threshold) /
-                                              magnitude_units_per_frequency_unit);
-
-      // Generate the next waveform
-      audio_buffer_next =
-          (audio_buffer_next == &audio_buffer_a) ? &audio_buffer_b : &audio_buffer_a;
-      Dac_WaveformToSamples(
-          Waveform_Square, SQUARE_DOMAIN_PERIOD, frequency, SQUARE_SCALE_FACTOR, audio_buffer_next);
-
-      // Play the next waveform
-      Dac_SetSource(audio_buffer_next);
-      Dac_Play();
-    }
-
-    // Wait until the next tick
-    next_tick_time += SYSTEM_PERIOD_MS;
-    BusyWaitUntil(next_tick_time);
-  }
+  Run();
 
   return 0;
 }
