fd377090e9d1daed6959859ed990a34c0a0d3743
Nicholas McCrea <nicholas.mccrea@colorado.edu>
2023-12-13 19:24:16 -0700

    Update the main loop to generate audio frequencyies proportional to acceleration.

--- source/main.c
+++ source/main.c
@@ -7,13 +7,19 @@
 #include "board.h"
 #include "busy_wait.h"
 #include "clock_config.h"
+#include "dac.h"
 #include "fsl_debug_console.h"
+#include "integer_test.h"
 #include "peripherals.h"
 #include "pin_mux.h"
 #include "systick.h"
+#include "waveform.h"
 
 // 50 ms period, or 20 Hz frequency
 #define SYSTEM_PERIOD_MS (50U)
+
+#define SCREAMO_MIN_OUTPUT_FREQUENCY (DAC_MIN_OUTPUT_FREQUENCY)
+#define SCREAMO_MAX_OUTPUT_FREQUENCY (2000)
 
 /**
  * @brief Application entry point.
@@ -26,17 +32,48 @@ int main(void)
   BOARD_InitBootPeripherals();
   BOARD_InitDebugConsole();
 
+  PRINTF("Running startup tests...\r\n");
+  if (IntegerTest() > 0) {
+    PRINTF("Startup tests failed. Exiting.\r\n");
+    return 1;
+  };
+
   SysTick_Initialize();
+  Dac_Initialize();
   Accelerometer_Initialize();
 
+  PRINTF("\r\n");
   PRINTF("Screamo started\r\n");
 
+  DacSamples_t dac_samples_a;
+  DacSamples_t dac_samples_b;
+  DacSamples_t* dac_samples_next = &dac_samples_a;
+
+  const uint32_t magnitude_units_per_frequency_unit =
+      ACCELEROMETER_MAX_MAGNITUDE / (SCREAMO_MAX_OUTPUT_FREQUENCY - SCREAMO_MIN_OUTPUT_FREQUENCY);
+
+  uint16_t frequency;
   ticktime_t next_tick_time = SysTick_Now();
   AccelerometerData_t accelerometer_data;
   while (1) {
     next_tick_time += SYSTEM_PERIOD_MS;
+
+    // Read the accelerometer data
     Accelerometer_Read(&accelerometer_data);
-    Accelerometer_PrintData(&accelerometer_data);
+
+    // Calculate the frequency to play
+    frequency = DAC_MIN_OUTPUT_FREQUENCY +
+                (accelerometer_data.magnitude / magnitude_units_per_frequency_unit);
+
+    // Generate the next waveform
+    dac_samples_next = (dac_samples_next == &dac_samples_a) ? &dac_samples_b : &dac_samples_a;
+    Dac_WaveformToSamples(
+        Waveform_Square, SQUARE_DOMAIN_PERIOD, frequency, SQUARE_SCALE_FACTOR, dac_samples_next);
+
+    // Play the next waveform
+    Dac_SetSource(dac_samples_next);
+    Dac_Play();
+
     BusyWaitUntil(next_tick_time);
   }
 
